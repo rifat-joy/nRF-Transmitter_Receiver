5dc61afa45ae30debb5d0bb9e4d1453940a54447
rifat-joy <56275477+rifat-joy@users.noreply.github.com>
2022-12-11 12:48:35 +0600

    Add files via upload

---
+++ Receiver/receiver/receiver.ino
@@ -0,0 +1,130 @@
+
+/* Receiver code for the Arduino Radio control with PWM output
+ * NRF24 library is required
+ * Module <-> NANO (5V,16MHz)
+    
+    GND    ->   GND
+    Vcc    ->   3.3V
+    CE     ->   D9
+    CSN    ->   D10
+    CLK    ->   D13
+    MOSI   ->   D11
+    MISO   ->   D12
+
+*This code receive signal for 7 channels and create a PWM output accordingly
+*/
+
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>  //To create PWM signals
+
+const uint64_t pipeIn = 0xE8E8F0F0E1LL;  //This is same in the transmitter
+RF24 radio(9, 10);                       //CSN and CE pins
+
+struct Received_data {
+  byte ch1;
+  byte ch2;
+  byte ch3;
+  byte ch4;
+  byte ch5;
+  byte ch6;
+  byte ch7;
+};
+
+Received_data received_data;
+
+Servo channel_1;
+Servo channel_2;
+Servo channel_3;
+Servo channel_4;
+Servo channel_5;
+Servo channel_6;
+Servo channel_7;
+
+int ch1_value = 0;
+int ch2_value = 0;
+int ch3_value = 0;
+int ch4_value = 0;
+int ch5_value = 0;
+int ch6_value = 0;
+int ch7_value = 0;
+
+
+unsigned long lastRecvTime = 0;
+
+void receive_the_data() {
+  while (radio.available()) {
+    radio.read(&received_data, sizeof(Received_data));
+    lastRecvTime = millis();  //Here we receive the data
+  }
+}
+
+void reset_the_Data() {
+  // Deafult values to use when NO radio input is detected
+  received_data.ch1 = 0;  //Throttle (channel 1) to 0
+  received_data.ch2 = 127;
+  received_data.ch3 = 127;
+  received_data.ch4 = 127;
+  received_data.ch5 = 0;
+  received_data.ch6 = 0;
+  received_data.ch7 = 0;
+}
+
+void setup() {
+  //Attaching the servo signal on pins from D2 to D8
+  channel_1.attach(2);
+  channel_2.attach(3);
+  channel_3.attach(4);
+  channel_4.attach(5);
+  channel_5.attach(6);
+  channel_6.attach(7);
+  channel_7.attach(8);
+
+  //reseting the received values
+  reset_the_Data();
+
+  //Once again, begining and radio configuration
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);  // NRF24 datarate Enum from library
+  radio.openReadingPipe(1, pipeIn);
+
+  //Starting of the radio comunication
+  radio.startListening();
+}
+
+void loop() {
+  //Receiving the radio data
+  receive_the_data();
+
+  //To reset if the data if signal is lost for 1 sec.
+  unsigned long now = millis();
+  if (now - lastRecvTime > 1000) {
+    // signal lost?
+    reset_the_Data();
+    lastRecvTime = millis();
+  }
+
+  ch1_value = map(received_data.ch1, 0, 255, 1000, 2000);
+  ch2_value = map(received_data.ch2, 0, 255, 1000, 2000);
+  ch3_value = map(received_data.ch3, 0, 255, 1000, 2000);
+  ch4_value = map(received_data.ch4, 0, 255, 1000, 2000);
+  ch5_value = map(received_data.ch5, 0, 1, 1000, 2000);
+  ch6_value = map(received_data.ch6, 0, 1, 1000, 2000);
+  ch7_value = map(received_data.ch7, 0, 255, 1000, 2000);
+
+  //Creathe the PWM signals
+  channel_1.writeMicroseconds(ch1_value);
+  channel_2.writeMicroseconds(ch2_value);
+  channel_3.writeMicroseconds(ch3_value);
+  channel_4.writeMicroseconds(ch4_value);
+  channel_5.writeMicroseconds(ch5_value);
+  channel_6.writeMicroseconds(ch6_value);
+  channel_7.writeMicroseconds(ch7_value);
+}
+
+
